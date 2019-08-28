15ce25e3c74e34a554e5879daefaa5e22a187df7
dengpeigen <dengpeigen@zywamail.com>
2019-08-29 01:29:16 +0800

    项目结构

---
+++ mi-web/src/main/java/com/dpg/mi/MiWebApplication.java
@@ -0,0 +1,14 @@
+package com.dpg.mi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dpg
+ */
+@SpringBootApplication
+public class MiWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MiWebApplication.class, args);
+    }
+}
